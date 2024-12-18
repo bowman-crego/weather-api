5a6d1c0df66bd1f091ab8dc151a47f64f9d02d0e
bowman-crego <bowmanccrego@gmail.com>
2024-12-18 12:28:15 -0800

    added some code

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,24 +1,28 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req, res) => {
- console.log(req, res);
+   
+  const cityName =req.body.cityName;
+  res.send(cityName);
+  
+ 
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
 router.get('/history', async (req, res) => {
-  console.log(req, res);
+  
 });
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => {
-  console.log(req, res);
+  
 });
 
 export default router;
